53437f4ab7bd78fd134633e0b9140cfeddde31c5
Lluke18 <lucianlesustefan@gmail.com>
2026-04-25 23:10:26 +0300

    all good

--- Scripts/room.gd
+++ Scripts/room.gd
@@ -5,7 +5,6 @@ extends Node
 
 @export var room_3d_path : String
 
-@export var mirror : InteractArea
 @export var left_room_path : String
 @export var right_room_path : String 
 
@@ -26,21 +25,12 @@ func _ready() -> void:
 		"I was just doing a reading and then...everything went sideways", 
 	    "And why are my photos all messed up?"
 	])
-	if mirror:
-		mirror.interact = Callable(self, "_on_mirror_change")
-
-func _on_mirror_change():
-	print("SWITCHING!")
-	
-	InteractionManager.unregister_area(mirror) # so it doesnt cause errors in 3d
-	SceneChanger.change_scene_to_path(room_3d_path)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("3D_left") and can_switch:
 		if left_room_path:
-			InteractionManager.unregister_area(mirror)
+
 			SceneChanger.change_scene_to_path(left_room_path)
 	if Input.is_action_just_pressed("3D_right") and can_switch:
 		if right_room_path:
-			InteractionManager.unregister_area(mirror)
 			SceneChanger.change_scene_to_path(right_room_path)
